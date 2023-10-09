2110829ae975902851ae6ed96788baaf72068604
Tapia-GJ <luivstapia285@gmail.com>
2023-10-09 15:32:16 -0500

    Agregar archivos de proyecto.

---
+++ 24CV_WEB/Controllers/EjemplosController1.cs
@@ -0,0 +1,21 @@
+﻿using _24CV_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_WEB.Controllers
+{
+    public class EjemplosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Tapia";
+            persona.Apellidos = "Rendón";
+            persona.FechaNacimiento = new DateTime(2003, 09, 15);
+            return View(persona);
+        }
+    }
+}
